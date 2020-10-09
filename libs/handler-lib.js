c4c48bdf1ca471cec953b3f314d223b1f0100dde
xzorphit <52004595+xzorphit@users.noreply.github.com>
2020-10-09 12:54:04 -0400

    Built to mock- Get Note API

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,18 +1,15 @@
 export default function handler(lambda) {
     return async function (event, context) {
-      let body, statusCode;
-  
-      try {
+      let body, statusCode;      try {
         // Run the Lambda
         body = await lambda(event, context);
         statusCode = 200;
       } catch (e) {
-        console.log(error)
+        console.log(e);
         body = { error: e.message };
         statusCode = 500;
       }
-  
-      // Return HTTP response
+      //Return HTTP response//
       return {
         statusCode,
         body: JSON.stringify(body),
